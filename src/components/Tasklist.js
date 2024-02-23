5e7d2bfca292cfcc19c74e23b4faa165924bfea8
rolemberg <vinnie.nsts@gmail.com>
2024-02-23 15:35:45 -0300

    Tasks component: spliting in components

--- src/components/Tasklist.js
+++ src/components/Tasklist.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { FaEdit, FaWindowClose } from 'react-icons/fa';
 import Form from "./Form";
+import Tasks from "./Tasks";
 
 export default class Main extends Component {
    constructor(props) {
@@ -25,29 +25,8 @@ export default class Main extends Component {
                   < Form 
                      writeTask={this.writeTask} novaTarefa={novaTarefa} addTask={this.addTask} />
 
-                  <ul itemID="sec-task" className="w-1/2 self-center py-10 list-disc flex flex-col-reverse">
-                     {
-                     
-                     tasks.map((task, i) => {
-                        return(
-                           <div className="flex flex-row justify-between p-5 transition-all ease-in-out duration-500 rounded-md hover:bg-slate-200">
-                              <li className="ml-2" key={i}>{task}</li>
+                  <Tasks tasks={tasks} handleEdit={this.handleEdit} handleDelete={this.handleDelete} />
 
-                              <div className="flex flex-row">
-
-                                 <FaEdit onClick={e => this.handleEdit(e, i)} 
-                                    className="mx-5 text-xl text-blue-800 hover:cursor-pointer hover:text-blue-900" />
-
-                                 <FaWindowClose onClick={e => this.handleDelete(e, i)} 
-                                    className="text-xl text-red-800 hover:cursor-pointer hover:text-red-900" />
-
-                              </div>
-                           </div>
-                        );
-                     })
-                     
-                     }
-                  </ul>
                </section>
             </div>
          </section>
---
+++ src/components/Tasks/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
+export default function Tasks({ tasks, handleEdit, handleDelete }) {
+   return(
+      <ul itemID="sec-task" className="w-1/2 self-center py-10 list-disc flex flex-col-reverse">
+         {
+         
+         tasks.map((task, i) => {
+            return(
+               <div className="flex flex-row justify-between p-5 transition-all ease-in-out duration-500 rounded-md hover:bg-slate-200">
+                  <li className="ml-2" key={i}>{task}</li>
+
+                  <div className="flex flex-row">
+
+                     <FaEdit onClick={e => handleEdit(e, i)} 
+                        className="mx-5 text-xl text-blue-800 hover:cursor-pointer hover:text-blue-900" />
+
+                     <FaWindowClose onClick={e => handleDelete(e, i)} 
+                        className="text-xl text-red-800 hover:cursor-pointer hover:text-red-900" />
+
+                  </div>
+               </div>
+            );
+         })
+         
+         }
+      </ul>
+   );
+};
+
